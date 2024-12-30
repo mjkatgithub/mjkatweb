312b50e734db70efa491d55bd8b2144cb135e58b
mjkatgithub <melchior.kannengiesser@gmx.de>
2024-12-31 00:01:56 +0100

    add some default layout

--- app.vue
+++ app.vue
@@ -1,21 +1,44 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/datenschutzerklaerung">datenschutzerklaerung</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/impressum">impressum</NuxtLink>
-        </li>
-      </ul>
-    </nav>
+  <div class="pageWrapper">
+    <header>
+      <NuxtLink to="/">Lo Go</NuxtLink>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/datenschutzerklaerung">datenschutzerklaerung</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/impressum">impressum</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <NuxtPage />
+    <footer>
+      footer
+    </footer>
   </div>
 </template>
+
+<style>
+  body {
+    background-color: #213c5b;
+  }
+  header {
+    background-color: #0c5688;
+  }
+
+  .pageWrapper {
+    background-color: #415c7b;
+  }
+
+  footer {
+    background-color: #26384c;
+  }
+</style>
