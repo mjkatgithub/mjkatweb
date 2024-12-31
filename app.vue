e4d2cef2d78a0e0613927979cff4f272afb3ecf7
mjkatgithub <melchior.kannengiesser@gmx.de>
2024-12-31 23:01:43 +0100

    more styling

--- app.vue
+++ app.vue
@@ -1,44 +1,189 @@
 <template>
   <div class="pageWrapper">
-    <header>
-      <NuxtLink to="/">Lo Go</NuxtLink>
-      <nav>
-        <ul>
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/datenschutzerklaerung">datenschutzerklaerung</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/impressum">impressum</NuxtLink>
-          </li>
+    <header class="pageHeader">
+      <div class="branding">
+        <NuxtLink to="/" class="logo">mjk @ web</NuxtLink>
+      </div>
+      <!-- Navigation -->
+      <nav :class="{ active: isMenuOpen }">
+        <ul class="mainNav">
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/links">Links</NuxtLink></li>
+          <li><NuxtLink to="/datenschutzerklaerung">Datenschutz-Erklärung</NuxtLink></li>
+          <li><NuxtLink to="/impressum">Impressum</NuxtLink></li>
         </ul>
       </nav>
+      <!-- Hamburger Menu -->
+      <div class="hamburgerMenu" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
-    <NuxtPage />
-    <footer>
-      footer
-    </footer>
+    <div class="pageContent">
+      <NuxtPage />
+    </div>
+    <footer>footer</footer>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
+
 <style>
-  body {
-    background-color: #213c5b;
+/* Mobile-First Design */
+
+/* Basis-Styling für mobile Geräte */
+body {
+  background-color: #213c5b;
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+.pageWrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 1024px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.pageHeader {
+  color: black;
+  min-height: 56px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* padding: 0.5rem 1rem; */
+  padding-left: 1rem;
+  padding-right: 1rem;
+  background: #1e6695;
+  background: -moz-linear-gradient(top, #1e6695 0%, #74aacc 33%, #79aecb 66%, #2f4a66 100%);
+  background: -webkit-linear-gradient(top, #1e6695 0%, #74aacc 33%, #79aecb 66%, #2f4a66 100%);
+  background: linear-gradient(to bottom, #1e6695 0%, #74aacc 33%, #79aecb 66%, #2f4a66 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e6695', endColorstr='#2f4a66',GradientType=0 );
+  /* border-top: 1px solid black; */
+}
+
+/* Branding */
+.branding .logo {
+  font-size: 1.5rem;
+  text-decoration: none;
+  color: black;
+}
+
+/* Navigation */
+nav {
+  display: none;
+}
+.mainNav {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  list-style: none;
+  margin: 0;
+  padding: 1rem;
+  background-color: #819cbb;
+  border-radius: 0.5rem;
+}
+.mainNav li a {
+  text-decoration: none;
+  color: black;
+  transition: color 0.3s;
+}
+
+/* Hamburger Menu */
+.hamburgerMenu {
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  cursor: pointer;
+}
+.hamburgerMenu span {
+  width: 25px;
+  height: 3px;
+  background-color: black;
+}
+
+/* Aktives Menü (für Mobile) */
+nav.active {
+  display: block;
+}
+
+.pageContent {
+  background-color: #415c7b;
+  padding: 15px;
+}
+
+.pageContent article {
+  border: 1px solid #000;
+  border-radius: 10px;
+  background-color: #819cbb;
+  padding: 15px;
+}
+
+footer {
+  height: 38px;
+  line-height: 38px;
+  text-align: center;
+  background: #415c7b;
+  background: -moz-linear-gradient(top, #415c7b 0%, #273749 100%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #415c7b), color-stop(100%, #273749));
+  background: -webkit-linear-gradient(top, #415c7b 0%, #273749 100%);
+  background: -o-linear-gradient(top, #415c7b 0%, #273749 100%);
+  background: -ms-linear-gradient(top, #415c7b 0%, #273749 100%);
+  background: linear-gradient(to bottom, #415c7b 0%, #273749 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#415c7b', endColorstr='#273749',GradientType=0 );
+  border-bottom: 1px solid black;
+}
+
+/* Media Queries für größere Bildschirme */
+@media (min-width: 600px) {
+  .mainNav {
+    flex-direction: row;
+    justify-content: center;
+    background: none;
+    padding: 0;
   }
-  header {
-    background-color: #0c5688;
+  nav {
+    display: flex;
+  }
+  .hamburgerMenu {
+    display: none;
+  }
+}
+
+@media (min-width: 1024px) {
+
+  .pageHeader {
+    margin-top: 10px;
+    -webkit-border-radius: 20px 20px 0 0;
+    border-radius: 20px 20px 0 0;
+    border: 1px solid black;
+    border-bottom: 0px;
   }
 
-  .pageWrapper {
-    background-color: #415c7b;
+  .pageContent {
+    border-left: solid 1px black;
+    border-right: solid 1px black;
   }
 
   footer {
-    background-color: #26384c;
+    -webkit-border-radius: 0 0 20px 20px;
+    border-radius: 0 0 20px 20px;
+    border: 1px solid black;
+    border-top: 0px;
   }
+}
 </style>
