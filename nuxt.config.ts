bc56e53f757f6473e0a0a7d164d4e0694da49f11
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-01-01 17:50:10 +0100

    enable tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss'
+  ]
 })
