1491a91625793aee3bd4ddcffa2b4e8c6ba16da3
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-01-03 19:19:11 +0100

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,13 @@ export default defineNuxtConfig({
     highlight: {
       theme: 'github-dark'
     }
+  },
+  // Base URL für GitHub Pages
+  router: {
+    base: '/mjkatweb/' // Passe dies an den Namen deines Repositories an
+  },
+  // Optional: Output Directory für statische Dateien
+  generate: {
+    dir: 'dist' // Standard ist 'dist', aber du kannst es anpassen
   }
 })
