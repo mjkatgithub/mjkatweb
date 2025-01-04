28d314d333a1b56b23b8f63ac54e09b6c3e659d2
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-01-04 01:16:17 +0100

    bla

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,22 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ],
-
   content: {
     highlight: {
       theme: 'github-dark'
     }
   },
-
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL || '/', // Set the base URL for the application
     cdnURL: process.env.NUXT_APP_BASE_URL ? `${process.env.NUXT_APP_BASE_URL}_nuxt/` : '/' // Set the CDN URL for static assets
   },
-
   nitro: {
     prerender: {
       routes: ['/']
     }
   },
-
   compatibilityDate: '2025-01-04'
 })
